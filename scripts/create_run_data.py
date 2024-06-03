8f61646006b0ea1f1104ec2ae0124db2e41d70a0
stefandnfr <dendorferstefan@gmail.com>
2024-06-03 17:25:46 +0200

    Add license

--- scripts/create_run_data.py
+++ scripts/create_run_data.py
@@ -1,4 +1,5 @@
 # Copyright 2019 The Google Research Authors.
+# Copyright [2024] Stefan Dendorfer
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 '''
 This python script generates run data using the NAS-Bench 101 tabular dataset. 
 It implements a regularized evolution algorithm for a fixed time budget and 
